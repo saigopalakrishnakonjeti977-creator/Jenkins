8f9511150ffeb50cb0d4db50d25646a81305c705
sai <saigopalakrishnakonjeti977@gmail.com>
2026-03-06 18:58:59 +0530

    added docker file

--- src/main/java/com/pipline/jenkins/JenkinsController.java
+++ src/main/java/com/pipline/jenkins/JenkinsController.java
@@ -1,14 +1,17 @@
 package com.pipline.jenkins;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("jenkins")
 public class JenkinsController {
+    @GetMapping("welcome")
     public String message(){
         return "Welcome to Jenkins Pipeleine";
     }
+    @GetMapping("hello")
     public String hello(){
         return "hello Jenkins";
     }
